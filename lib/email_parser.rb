34dd9e44d3a79f833a1be9a16e38fccdf587eda5
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-04 05:07:44 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailParser
   def parse
     mails = Array.new
     if @emails.include?(",") and @emails.include?(".com ")
-      mails = @emails.split("/,\s\s/")
+      mails = @emails.split("/,\s/")
     end
     if @emails.include?(",")
       mails = @emails.split(", ")
